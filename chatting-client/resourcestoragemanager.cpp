328bb12b02ec651d4ea3c1865e9af69a690c6197
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-03-30 14:57:22 +1100

    1.[ADD]:add resourcestoragemanager singleton class to store resource's ip:port and user id info(not impled yet)

---
+++ chatting-client/resourcestoragemanager.cpp
@@ -0,0 +1,14 @@
+#include "resourcestoragemanager.h"
+
+ResourceStorageManager::~ResourceStorageManager()
+{
+
+}
+
+ResourceStorageManager::ResourceStorageManager()
+    :m_info{}
+{}
+
+void ResourceStorageManager::setUserInfo(std::shared_ptr<UserNameCard> info) {
+    m_userInfo = info;
+}

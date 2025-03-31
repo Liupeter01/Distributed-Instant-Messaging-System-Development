9788bdc9b8a09fc41fe7a329671a581d94aa283d
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-03-31 20:34:25 +1100

    1.[ADD]:add subthread to maintain connection in other threads, because we need to update progress bar, while user is still using the filetransferdialog 2.[ADD]:add logicexecutor base on QObject obj, no need to maintain memory dealloc 3.[ADD]:add registersignal function

--- chatting-client/logicmethod.h
+++ chatting-client/logicmethod.h
@@ -2,8 +2,8 @@
 #define LOGICMETHOD_H
 
 #include <QObject>
+#include <QThread>
 #include <singleton.hpp>
-
 /*execute some specfic logic from network*/
 #include <logicexecutor.h>
 
@@ -19,11 +19,21 @@ public:
 
 private:
     explicit LogicMethod(QObject *parent = nullptr);
+    void registerSignals();
 
 signals:
+    /*forward resources server's message to a standlone logic thread*/
+    void signal_resources_logic_handler(const uint16_t id, const QJsonObject obj);
+
+    /*data transmission status*/
+    void signal_data_transmission_status(const QString& filename,
+                                         const std::size_t curr_seq,
+                                         const std::size_t curr_size,
+                                         const std::size_t total_size);
 
 private:
-
+    QThread * m_thread;
+    LogicExecutor* m_exec;
 };
 
 #endif // LOGICMETHOD_H

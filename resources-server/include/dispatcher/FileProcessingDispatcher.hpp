89953da46b84dd4a7da4e9c9d3bc2bceea7cf118
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-04-03 13:44:36 +1100

    1.[ADD]:add an concurrent vector of FileProcessingDispatcher to process network event, also concurrent vector could support tasks stealing in the future for load balance purposes 2.[ADD]:add hash algo to distributed task to each nodes according to session is 3.[IMPL]:ass std::shared_ptr based session parameter to extend it's life cycle to prevent it from destruction

--- resources-server/include/dispatcher/FileProcessingDispatcher.hpp
+++ resources-server/include/dispatcher/FileProcessingDispatcher.hpp
@@ -1,20 +1,53 @@
 #pragma once
 #ifndef _FILE_PROCESSING_DISPATCHER_HPP_
 #define _FILE_PROCESSING_DISPATCHER_HPP_
+#include <memory>
+#include <tbb/concurrent_vector.h>
 #include <handler/FileProcessingNode.hpp>
 #include <singleton/singleton.hpp>
+
+namespace handler {
+          class FileDescriptionBlock;
+}
 
 namespace dispatcher {
 
 class FileProcessingDispatcher : public Singleton<FileProcessingDispatcher> {
-  friend class Singleton<FileProcessingDispatcher>;
 
-  FileProcessingDispatcher();
+  friend class Singleton<FileProcessingDispatcher>;
+  using SessionPtr = std::shared_ptr<Session>;
+  using FPTRType = std::shared_ptr<handler::FileProcessingNode>;
+  using ContainerType =
+            tbb::concurrent_vector<FPTRType>;
 
 public:
   virtual ~FileProcessingDispatcher();
+  void shutdown();
+
+  void commit(std::unique_ptr<handler::FileDescriptionBlock> block,
+            [[maybe_unused]] SessionPtr live_extend);
+
+  void commit(const std::string& filename, const std::string& block_data,
+            const std::string& checksum, const std::string& curr_sequence,
+            const std::string& last_sequence, std::size_t accumlated_size,
+            std::size_t file_size, [[maybe_unused]] SessionPtr live_extend);
+
+protected:
+          const std::size_t hash_to_index(std::string_view session_id) const;
 
 private:
+          FileProcessingDispatcher();
+          FileProcessingDispatcher(std::size_t threads);
+
+          [[nodiscard]] typename ContainerType::iterator 
+          dispatch_to_iterator(std::string_view session_id);
+
+          [[nodiscard]] std::optional<FPTRType>
+         dispatch_to_node(std::string_view session_id);
+
+private:
+          std::hash<std::string_view> m_convertor;
+          ContainerType m_nodes;
 };
 
 } // namespace dispatcher
--- resources-server/src/FileProcessingDispatcher.cpp
+++ resources-server/src/FileProcessingDispatcher.cpp
@@ -1,5 +1,77 @@
+#include <algorithm>
+#include <spdlog/spdlog.h>
 #include <dispatcher/FileProcessingDispatcher.hpp>
 
-dispatcher::FileProcessingDispatcher::FileProcessingDispatcher() {}
+dispatcher::FileProcessingDispatcher::FileProcessingDispatcher()
+          :FileProcessingDispatcher(std::thread::hardware_concurrency() < 2
+          ? 2
+          : std::thread::hardware_concurrency()) {}
 
-dispatcher::FileProcessingDispatcher::~FileProcessingDispatcher() {}
+dispatcher::FileProcessingDispatcher::FileProcessingDispatcher(std::size_t threads) 
+          : m_convertor(){
+          for (std::size_t thread = 0; thread < threads; ++thread) {
+                    m_nodes.emplace_back(std::make_shared<handler::FileProcessingNode>(thread));
+          }
+}
+
+dispatcher::FileProcessingDispatcher::~FileProcessingDispatcher() { shutdown(); }
+
+void dispatcher::FileProcessingDispatcher::shutdown() {
+          std::for_each(m_nodes.begin(), m_nodes.end(), 
+                    [](std::shared_ptr<handler::FileProcessingNode>& node) {
+                              node->shutdown();
+                    });
+}
+
+void dispatcher::FileProcessingDispatcher::commit(std::unique_ptr<handler::FileDescriptionBlock> block,
+                                                                                [[maybe_unused]] SessionPtr live_extend) {
+
+          auto filename = block->filename;
+
+          // if opt has value then it could be executed by this if condition
+          if (auto opt = dispatch_to_node(filename); opt) {
+                    (*opt)->commit(std::move(block), live_extend);
+                    spdlog::info("[Resources Server]: Dispatcher File Processing Task To Node {} "
+                              "Successfully!",
+                              (*opt)->getProcessingId());
+                    return;
+          }
+
+          spdlog::error(
+                    "[Resources Server]: Dispatcher Error While Handling Session: {}",
+                    filename);
+}
+
+void dispatcher::FileProcessingDispatcher::commit(const std::string& filename, const std::string& block_data,
+                                                                                const std::string& checksum, const std::string& curr_sequence,
+                                                                                const std::string& last_sequence, std::size_t accumlated_size,
+                                                                                std::size_t file_size, [[maybe_unused]] SessionPtr live_extend) {
+
+          commit(std::make_unique<handler::FileDescriptionBlock>(filename, block_data, checksum,
+                    curr_sequence, last_sequence,
+                    accumlated_size, file_size),
+                    live_extend);
+}
+
+const std::size_t dispatcher::FileProcessingDispatcher::hash_to_index(
+          std::string_view session_id) const {
+          return m_convertor(session_id) % m_nodes.size();
+}
+
+dispatcher::FileProcessingDispatcher::ContainerType::iterator
+dispatcher::FileProcessingDispatcher::dispatch_to_iterator(
+          std::string_view session_id) {
+          if (m_nodes.empty()) {
+                    return m_nodes.end(); // or throw
+          }
+
+          return m_nodes.begin() + hash_to_index(session_id);
+}
+
+std::optional<typename dispatcher::FileProcessingDispatcher::FPTRType>
+dispatcher::FileProcessingDispatcher::dispatch_to_node(
+          std::string_view session_id) {
+          if (m_nodes.empty())
+                    return std::nullopt;
+          return m_nodes[m_convertor(session_id) % m_nodes.size()];
+}

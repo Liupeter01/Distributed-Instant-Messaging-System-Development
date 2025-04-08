6aee9fc6291c85b627ab310638959dd9702fc2b8
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-04-08 20:48:16 +1000

    1.[UPDATE]:update resources server log sys 2.[REMOVE]:remove unnessary unordered_map+mutex

--- resources-server/include/server/AsyncServer.hpp
+++ resources-server/include/server/AsyncServer.hpp
@@ -1,9 +1,7 @@
 #pragma once
 #ifndef _ASYNCSERVER_HPP_
 #define _ASYNCSERVER_HPP_
-#include <mutex>
 #include <server/Session.hpp>
-#include <unordered_map>
 
 class SyncLogic;
 
@@ -19,25 +17,16 @@ public:
   void startAccept();
 
 private:
-  void terminateConnection(const std::string &session_id);
+  void terminateConnection(const std::string& user_uuid);
   void handleAccept(std::shared_ptr<Session> session,
                     boost::system::error_code ec);
 
 private:
-  /*create a mutex to protect m_sessions*/
-  std::mutex m_mtx;
-
   /*boost io_context*/
   boost::asio::io_context &m_ioc;
 
   /*create a server acceptor to accept connection*/
   boost::asio::ip::tcp::acceptor m_acceptor;
-
-  /*maintain user's connection*/
-  std::unordered_map<
-      /*unique_string*/ std::string,
-      /*connected session*/ std::shared_ptr<Session>>
-      m_sessions;
 };
 
 #endif

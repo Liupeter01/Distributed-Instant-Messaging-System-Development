510398433309a3a6c8d23af10272a6279cc13d9f
Liupeter01 <Liupeter012001@outlook.com>
2025-04-09 19:54:14 +1000

    1.[UPDATE]:update concurrent unordered map with hash map because of the unsafe issue in unordered_map 2.[ADD]:add sfinae code to test if the container is specific data type 3.[FORMATTING]

--- balance-server/include/grpc/GrpcDataLayer.hpp
+++ balance-server/include/grpc/GrpcDataLayer.hpp
@@ -1,96 +1,131 @@
 #pragma once
 #ifndef _GRPCDATALAYER_HPP_
 #define _GRPCDATALAYER_HPP_
-#include <memory>
 #include <atomic>
+#include <memory>
+#include <mutex>
 #include <network/def.hpp>
 #include <optional>
-#include <tbb/concurrent_unordered_map.h>
 #include <singleton/singleton.hpp>
+#include <tbb/concurrent_hash_map.h>
+#include <type_traits>
+#include <spdlog/spdlog.h>
 
 namespace grpc {
 
-          class GrpcUserServiceImpl;
+class GrpcUserServiceImpl;
+class GrpcChattingImpl;
+class GrpcResourcesImpl;
 
-          namespace details {
+namespace details {
 
-                    struct ServerInstanceConf {
-                              ServerInstanceConf(const std::string& host, const std::string& port,
-                                        const std::string& name);
+struct ServerInstanceConf {
+  ServerInstanceConf(const std::string &host, const std::string &port,
+                     const std::string &name);
 
-                              std::string _host;
-                              std::string _port;
-                              std::string _name;
-                              std::atomic<std::size_t> _connections = 0; /*add init*/
-                    };
+  std::string _host;
+  std::string _port;
+  std::string _name;
+  std::atomic<std::size_t> _connections = 0; /*add init*/
+};
 
-                    struct GRPCServerConf {
-                              GRPCServerConf(const std::string& host, const std::string& port,
-                                        const std::string& name);
-                              std::string _host;
-                              std::string _port;
-                              std::string _name;
-                    };
+struct GRPCServerConf {
+  GRPCServerConf(const std::string &host, const std::string &port,
+                 const std::string &name);
+  std::string _host;
+  std::string _port;
+  std::string _name;
+};
 
-                    enum class SERVER_TYPE {
-                              CHATTING_SERVER,
-                              RESOURCES_SERVER
-                    };
+//forward declaration
+class GrpcDataLayer;
 
-                    class GrpcDataLayer :public Singleton<GrpcDataLayer>{
-                              friend class Singleton<GrpcDataLayer>;
-                              friend class grpc::GrpcUserServiceImpl;
-                              GrpcDataLayer() = default;
+template<typename Container>
+struct is_valid_mapping_type : std::false_type {};
 
-                    public:
-                              virtual ~GrpcDataLayer() = default;
+template<>
+struct is_valid_mapping_type<GrpcDataLayer::InstancesMappingType> : std::true_type {};
 
-                    protected:
-                              std::optional<std::shared_ptr<grpc::details::ServerInstanceConf>> 
-                               serverInstanceLoadBalancer(SERVER_TYPE type = SERVER_TYPE::CHATTING_SERVER);
+template<>
+struct is_valid_mapping_type<GrpcDataLayer::GrpcMappingType> : std::true_type {};
 
-                              void registerUserInfo(const std::size_t uuid, const std::string& tokens);
+enum class SERVER_TYPE {
+  CHATTING_SERVER_INSTANCE,
+  RESOURCES_SERVER_INSTANCE,
+  CHATTING_GRPC_SERVER,
+  RESOURCES_GRPC_SERVER
+};
 
-                              /*get user token from Redis*/
-                              std::optional<std::string> getUserToken(const std::size_t uuid);
-                              ServiceStatus verifyUserToken(const std::size_t uuid, const std::string& tokens);
+class GrpcDataLayer : public Singleton<GrpcDataLayer> {
+  friend class Singleton<GrpcDataLayer>;
+  friend class grpc::GrpcUserServiceImpl;
+  GrpcDataLayer() = default;
 
-                    private:
-                              std::optional<std::shared_ptr<grpc::details::ServerInstanceConf>>
-                              chattingInstanceLoadBalancer();
+public:
+  using InstancesMappingType = tbb::concurrent_hash_map<
+      /*server name*/ std::string,
+      /*server info*/ std::unique_ptr<grpc::details::ServerInstanceConf>>;
 
-                              std::optional<std::shared_ptr<grpc::details::ServerInstanceConf>>
-                               resourcesInstanceLoadBalancer();
+  using GrpcMappingType = tbb::concurrent_hash_map<
+      /*server name*/ std::string,
+      /*server info*/ std::unique_ptr<grpc::details::GRPCServerConf>>;
 
-                    private:
-                              /*redis*/
-                              const std::string redis_server_login = "redis_server";
+public:
+  virtual ~GrpcDataLayer() = default;
 
-                              /*user token predix*/
-                              const std::string token_prefix = "user_token_";
+protected:
+  auto &getChattingServerInstances() { return m_chattingServerInstances; }
+  auto &getChattingGRPCServer() { return m_chattingGRPCServer; }
+  auto &getResourcesServerInstances() { return m_resourcesServerInstances; }
+  auto &getResourcesGRPCServer() { return m_resourcesGRPCServer; }
 
-                              
-                              tbb::concurrent_unordered_map<
-                                        /*server name*/ std::string,
-                                        /*server info*/ std::unique_ptr< grpc::details::ServerInstanceConf>>
-                                        m_chattingServerInstances;
+  bool removeItemFromServer(const std::string &server_name, SERVER_TYPE type);
 
-                              tbb::concurrent_unordered_map<
-                                        /*server name*/ std::string,
-                                        /*server info*/ std::unique_ptr< grpc::details::GRPCServerConf>>
-                                        m_chattingGRPCServer;
+  std::optional<std::shared_ptr<grpc::details::ServerInstanceConf>>
+  serverInstanceLoadBalancer(
+      SERVER_TYPE type = SERVER_TYPE::CHATTING_SERVER_INSTANCE);
 
-                              tbb::concurrent_unordered_map<
-                                        /*server name*/ std::string,
-                                        /*server info*/ std::unique_ptr< grpc::details::ServerInstanceConf>>
-                                        m_resourcesServerInstances;
+  void registerUserInfo(const std::size_t uuid, const std::string &tokens);
 
-                              tbb::concurrent_unordered_map<
-                                        /*server name*/ std::string,
-                                        /*server info*/ std::unique_ptr<  grpc::details::GRPCServerConf>>
-                                        m_resourcesGRPCServer;
-                    };
-          }
-}
+  /*get user token from Redis*/
+  std::optional<std::string> getUserToken(const std::size_t uuid);
+  ServiceStatus verifyUserToken(const std::size_t uuid,
+                                const std::string &tokens);
+
+private:
+  std::optional<std::shared_ptr<grpc::details::ServerInstanceConf>>
+  chattingInstanceLoadBalancer();
+
+  std::optional<std::shared_ptr<grpc::details::ServerInstanceConf>>
+  resourcesInstanceLoadBalancer();
+
+  template <typename Container,
+            std::enable_if_t<is_valid_mapping_type<Container>::value, int> = 0>
+  bool removeItemFromConcurrentHashMap(Container &container,
+                                       const std::string &server_name) {
+    typename Container::accessor accessor;
+    if (container.find(accessor, server_name)) {
+      /*remove the item from the container*/
+      container.erase(accessor);
+      return true;
+    }
+    return false;
+  }
+
+private:
+  /*redis*/
+  const std::string redis_server_login = "redis_server";
+
+  /*user token predix*/
+  const std::string token_prefix = "user_token_";
+
+  InstancesMappingType m_chattingServerInstances;
+  InstancesMappingType m_resourcesServerInstances;
+
+  GrpcMappingType m_chattingGRPCServer;
+  GrpcMappingType m_resourcesGRPCServer;
+};
+} // namespace details
+} // namespace grpc
 
 #endif

76f223a99ed2679db4ebfe68d1d43b81a1a8923e
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-04-09 21:58:56 +1000

    1.[UPDATE]:update namespace 2.[MODIFY]:modify pool name

--- gateway-server/src/main.cpp
+++ gateway-server/src/main.cpp
@@ -1,5 +1,5 @@
 #include <config/ServerConfig.hpp>
-#include <grpc/BalanceServicePool.hpp>
+#include <grpc/UserServicePool.hpp>
 #include <grpc/VerificationServicePool.hpp>
 #include <iostream>
 #include <redis/RedisManager.hpp>
@@ -14,7 +14,7 @@ int main() {
      * 2. MySQLConnectionPool
      * 3. RedisConnectionPool
      * 4. VerificationServicePool
-     * 5. BalancerServicePool
+     * 5. UserServicePool
      * */
     [[maybe_unused]] auto &service_pool = IOServicePool::get_instance();
     [[maybe_unused]] auto &sql = mysql::MySQLConnectionPool::get_instance();
@@ -22,7 +22,7 @@ int main() {
     [[maybe_unused]] auto &verification =
         stubpool::VerificationServicePool::get_instance();
     [[maybe_unused]] auto &balance =
-        stubpool::BalancerServicePool::get_instance();
+        stubpool::UserServicePool::get_instance();
 
     /*setting up signal*/
     boost::asio::io_context ioc;

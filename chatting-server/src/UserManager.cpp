76ff97ebbc6d460662cb75dac5ad95e114bcb09d
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-04-10 20:26:11 +1000

    1.[REFACTOR]:replace mutex+unordered_map to tbb::concurrent_hash_map for concurrency 2.[ADD]:add teminate function, when system shutdown, all connected client will receive logout service package and close socket connection 3.[ADD]:add static kick function for future useage

--- chatting-server/src/UserManager.cpp
+++ chatting-server/src/UserManager.cpp
@@ -1,5 +1,16 @@
-#include <server/Session.hpp>
+#include <network/def.hpp>
+#include <boost/json.hpp>
+#include <boost/json/object.hpp>
+#include <boost/json/parse.hpp>
 #include <server/UserManager.hpp>
+
+std::string UserManager::redis_server_login = "redis_server";
+
+/*store user base info in redis*/
+std::string UserManager::user_prefix = "user_info_";
+
+/*store the server name that this user belongs to*/
+std::string UserManager::server_prefix = "uuid_";
 
 UserManager::UserManager() {}
 
@@ -7,27 +18,58 @@ UserManager::~UserManager() { m_uuid2Session.clear(); }
 
 std::optional<std::shared_ptr<Session>>
 UserManager::getSession(const std::string &uuid) {
-  std::lock_guard<std::mutex> _lckg(m_update_mtx);
-  auto target = m_uuid2Session.find(uuid);
-  if (target == m_uuid2Session.end()) {
-    return std::nullopt;
-  }
-  return target->second;
+
+          typename ContainerType::const_accessor accessor;
+          if (m_uuid2Session.find(accessor, uuid)) {
+                    return accessor->second;
+          }
+          return std::nullopt;
 }
 
 void UserManager::removeUsrSession(const std::string &uuid) {
-
-  std::lock_guard<std::mutex> _lckg(m_update_mtx);
-  auto target = m_uuid2Session.find(uuid);
-  if (target != m_uuid2Session.end()) {
-    target->second->closeSession();
-    m_uuid2Session.erase(uuid);
+  typename ContainerType::accessor accessor;
+  if (m_uuid2Session.find(accessor, uuid)) {
+            /*remove the item from the container*/
+            accessor->second->closeSession();
+            m_uuid2Session.erase(accessor);
   }
 }
 
 void UserManager::alterUserSession(const std::string &uuid,
                                    std::shared_ptr<Session> session) {
-  std::lock_guard<std::mutex> _lckg(m_update_mtx);
-  m_uuid2Session.insert(
-      std::pair<std::string, std::shared_ptr<Session>>(uuid, session));
+
+          //safty consideration
+          removeUsrSession(uuid);
+
+          m_uuid2Session.insert(
+                    std::pair<std::string, std::shared_ptr<Session>>(uuid, session));
+}
+
+void UserManager::teminate() {
+
+          connection::ConnectionRAII<redis::RedisConnectionPool, redis::RedisContext>
+                    raii;
+
+          std::for_each(m_uuid2Session.begin(), m_uuid2Session.end(), 
+                    [this,  &raii](decltype(*m_uuid2Session.begin()) & pair) {
+
+                              UserManager::kick(raii, pair.second);
+                    });
+
+          m_uuid2Session.clear();
+}
+
+void UserManager::kick(RedisRAII& raii, std::shared_ptr<Session> session) {
+
+          boost::json::object logout;
+          logout["error"] = static_cast<std::size_t>(ServiceStatus::SERVICE_SUCCESS);
+          logout["uuid"] = session->get_user_uuid();
+
+          session->sendMessage(ServiceType::SERVICE_LOGOUTRESPONSE,
+                    boost::json::serialize(logout));
+
+          /*Remove from Redis*/
+          raii->get()->delPair(server_prefix + session->get_user_uuid());
+
+          session->closeSession();
 }

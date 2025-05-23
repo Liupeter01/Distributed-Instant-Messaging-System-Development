d2c841fac15267ccf1b42087757ebef9f645b3fe
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-05-23 21:42:08 +1000

    1.[UPDATE]:update usermanager according to the impl of chatting client 2.[COMMENT]:removeredis part was removed from the process

--- resources-server/include/server/UserManager.hpp
+++ resources-server/include/server/UserManager.hpp
@@ -2,17 +2,22 @@
 #ifndef _USERMANAGER_HPP_
 #define _USERMANAGER_HPP_
 #include <optional>
+#include <redis/RedisManager.hpp>
+#include <server/Session.hpp>
+#include <service/ConnectionPool.hpp>
 #include <singleton/singleton.hpp>
 #include <string>
 #include <tbb/concurrent_hash_map.h>
 
-/*declaration*/
-class Session;
+class AsyncServer;
 
 class UserManager : public Singleton<UserManager> {
   friend class Singleton<UserManager>;
+  friend class AsyncServer;
   UserManager();
 
+  using RedisRAII = connection::ConnectionRAII<redis::RedisConnectionPool,
+                                               redis::RedisContext>;
   using ContainerType = tbb::concurrent_hash_map<
       std::string,
       /*user belonged session*/ std::shared_ptr<Session>>;
@@ -20,14 +25,22 @@ class UserManager : public Singleton<UserManager> {
 public:
   ~UserManager();
   std::optional<std::shared_ptr<Session>> getSession(const std::string &uuid);
-  void removeUsrSession(const std::string &uuid);
-  void alterUserSession(const std::string &uuid,
-                        std::shared_ptr<Session> session);
+  bool removeUsrSession(const std::string &uuid);
+  bool removeUsrSession(const std::string &uuid, const std::string &session_id);
+  void createUserSession(const std::string &uuid,
+                         std::shared_ptr<Session> session);
+
+  /*we do not need to teminate the user at once, we just put them to another
+   * structure*/
+  bool moveUserToTerminationZone(const std::string &uuid);
 
 protected:
   void teminate();
+  void eraseWaitingSession(const std::string &uuid);
 
 private:
+          bool m_status;
   ContainerType m_uuid2Session;
+  ContainerType m_waitingToBeClosed;
 };
 #endif //_USERMANAGER_HPP_
--- resources-server/src/UserManager.cpp
+++ resources-server/src/UserManager.cpp
@@ -1,8 +1,9 @@
-#include <algorithm>
-#include <server/Session.hpp>
+#include <network/def.hpp>
 #include <server/UserManager.hpp>
 
-UserManager::UserManager() {}
+UserManager::UserManager() 
+          : m_status(true)
+{}
 
 UserManager::~UserManager() { teminate(); }
 
@@ -16,30 +17,100 @@ UserManager::getSession(const std::string &uuid) {
   return std::nullopt;
 }
 
-void UserManager::removeUsrSession(const std::string &uuid) {
+bool UserManager::removeUsrSession(const std::string &uuid) {
 
   typename ContainerType::accessor accessor;
-  if (m_uuid2Session.find(accessor, uuid)) {
-    /*remove the item from the container*/
-    accessor->second->closeSession();
-    m_uuid2Session.erase(accessor);
+  if (!m_waitingToBeClosed.find(accessor, uuid))
+    return false;
+
+  auto session = accessor->second;
+
+  // Mark as delete this session later
+  // maybe it still has some other commitments to do
+  session->markAsDeferredTerminated([this, uuid, session]() {
+    session->closeSession();
+    eraseWaitingSession(uuid);
+  });
+
+  return true;
+}
+
+bool UserManager::removeUsrSession(const std::string &uuid,
+                                   const std::string &session_id) {
+
+  typename ContainerType::accessor accessor;
+  if (!m_waitingToBeClosed.find(accessor, uuid))
+    return false;
+
+  auto session = accessor->second;
+  if (session->get_session_id() != session_id) {
+    spdlog::warn(
+        "[{}] removeUsrSession called with mismatched session_id for UUID {}",
+        ServerConfig::get_instance()->GrpcServerName, uuid);
+    return false;
+  }
+
+  // Mark as delete this session later
+  // maybe it still has some other commitments to do
+  session->markAsDeferredTerminated([this, uuid, session]() {
+    session->closeSession();
+    eraseWaitingSession(uuid);
+  });
+
+  return true;
+}
+
+void UserManager::eraseWaitingSession(const std::string &uuid) {
+  typename ContainerType::accessor erase_accessor;
+  if (m_waitingToBeClosed.find(erase_accessor, uuid)) {
+    m_waitingToBeClosed.erase(erase_accessor);
   }
 }
 
-void UserManager::alterUserSession(const std::string &uuid,
-                                   std::shared_ptr<Session> session) {
-
-  // safty consideration
-  removeUsrSession(uuid);
-
+void UserManager::createUserSession(const std::string &uuid,
+                                    std::shared_ptr<Session> session) {
   m_uuid2Session.insert(
       std::pair<std::string, std::shared_ptr<Session>>(uuid, session));
 }
 
+bool UserManager::moveUserToTerminationZone(const std::string &uuid) {
+
+  typename ContainerType::accessor accessor;
+  if (!m_uuid2Session.find(accessor, uuid))
+    return false;
+
+  typename ContainerType::accessor close_accessor;
+  m_waitingToBeClosed.insert(close_accessor, uuid);
+  close_accessor->second = std::move(accessor->second);
+  m_uuid2Session.erase(accessor);
+  return true;
+}
+
 void UserManager::teminate() {
-  std::for_each(m_uuid2Session.begin(), m_uuid2Session.end(),
-                [](decltype(*m_uuid2Session.begin()) &pair) {
-                  pair.second->closeSession();
-                });
-  m_uuid2Session.clear();
+
+          if (m_status) {
+                    /* record  session's uuid, and we deal with them later*/
+                    std::vector<std::string> to_be_terminated;
+
+                    // copy original session to a duplicate one
+                    UserManager::ContainerType& lists =
+                              UserManager::get_instance()->m_uuid2Session;
+
+                    for (auto& client : lists) {
+                              // Ask the client to be offlined, and move it to waitingToBeClosed queue
+                              client.second->sendOfflineMessage();
+                              //client.second->removeRedisCache(client.second->get_user_uuid(),
+                              //          client.second->get_session_id());
+
+                              // collect expired client info, and we process them later!
+                              to_be_terminated.push_back(client.first);
+                    }
+
+                    for (const auto& gg : to_be_terminated) {
+                              UserManager::get_instance()->moveUserToTerminationZone(gg);
+                              UserManager::get_instance()->removeUsrSession(gg);
+                    }
+
+                    m_status = false;
+          }
 }

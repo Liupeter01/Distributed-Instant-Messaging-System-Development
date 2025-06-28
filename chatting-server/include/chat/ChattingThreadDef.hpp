d5fd7e3546cd57237012a44ba6ab5cb89c95ceb9
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-06-28 12:59:19 +1000

    1.[ADD]:add  ChattingThreadDesc with _msg-msg_id

--- chatting-server/include/chat/ChattingThreadDef.hpp
+++ chatting-server/include/chat/ChattingThreadDef.hpp
@@ -47,6 +47,58 @@ namespace chat {
                     std::optional<std::string> _user_one = std::nullopt;
                     std::optional<std::string> _user_two = std::nullopt;
           };
+
+          struct ChattingThreadDesc {
+
+                    ChattingThreadDesc() = default;
+                    ChattingThreadDesc(const ChatThreadMeta& o)
+                              :_meta(o._thread_id, o._chat_type, o._user_one, o._user_two)
+                              , _last_msg_id(std::nullopt)
+                    {
+                    }
+
+                    ChattingThreadDesc(const std::string& __thread_id,
+                              const UserChatType _type,
+                              std::optional < std::string> __last_msg_id = std::nullopt,
+                              std::optional<std::string> one = std::nullopt,
+                              std::optional<std::string> two = std::nullopt)
+                              :_meta(__thread_id, _type, one, two)
+                              , _last_msg_id(__last_msg_id)
+                    {
+                    }
+
+                    bool isGroupChat() const {
+                              return _meta.isGroupChat();
+                    }
+                    const std::string& getThreadId()const {
+                              return _meta._thread_id;
+                    }
+                    std::optional < std::string> getLastMsgId() const {
+                              return _last_msg_id;
+                    }
+                    std::optional < std::string> getUserOneUUID() const {
+                              return _meta._user_one;
+                    }
+                    std::optional < std::string> getUserTwoUUID() const {
+                              return _meta._user_two;
+                    }
+
+                    void setLastMsgId(const std::string& last) {
+                              _last_msg_id = last;
+                    }
+
+                    static ChattingThreadDesc createPrivateChat(const std::string& thread_id,
+                              const std::string& user_one,
+                              const std::string& user_two) {
+                              return ChattingThreadDesc(thread_id, UserChatType::PRIVATE, std::nullopt, user_one, user_two);
+                    }
+
+                    static ChattingThreadDesc createGroupChat(const std::string& thread_id) {
+                              return ChattingThreadDesc(thread_id, UserChatType::GROUP);
+                    }
+                    ChatThreadMeta _meta;
+                    std::optional < std::string> _last_msg_id;     //last msg id for data retrieve!
+          };
 }
 
 #endif //_CHATTINGTHREADDEF_H

40f23d84c13fdaa4f1e13287b1a78358779c1026
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-08-30 13:49:44 +1000

    1.[ADD]:add mysql testdata for quick database init

---
+++ conf/CreateTestData.sql
@@ -0,0 +1,6 @@
+INSERT INTO chatting.Authentication(username, password, email) VALUES ("test_user_a", "1#Ludhyno^&NkB5l", "email");
+INSERT INTO chatting.Authentication(username, password, email) VALUES ("test_user_b", "*x%n$c8sB#8sBRG!", "email");
+INSERT INTO chatting.Authentication(username, password, email) VALUES ("test_user_c", "W3YsH7BoOBi&s7EY", "email");
+INSERT INTO chatting.UserProfile(uuid, avatar, nickname, description, sex) VALUES (1, "1.png", "c++", "hello c++", 0);
+INSERT INTO chatting.UserProfile(uuid ,avatar, nickname, description, sex) VALUES (2, "2.png", "python", "hello python", 1);
+INSERT INTO chatting.UserProfile(uuid ,avatar, nickname, description, sex) VALUES (3, "3.png", "nodejs", "hello nodejs", 1);

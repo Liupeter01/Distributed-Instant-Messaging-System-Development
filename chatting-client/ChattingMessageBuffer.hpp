4b66796de19ac185ef71423e53f9affb56b4b2e1
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-09-01 22:18:20 +1000

    1.[MODIFY]:all messages are stored inside a linklist for sequence consideration, if the server auth the request and grant local unique_id to msg_id then we could move it to another mapping struture 2.[ALGO]:based on LRU std::list + unordered_map/map 3.[IMPROVE]:introduce rollback in _insert

--- chatting-client/ChattingMessageBuffer.hpp
+++ chatting-client/ChattingMessageBuffer.hpp
@@ -14,80 +14,107 @@ public:
   MessageBuffer() = default;
   virtual ~MessageBuffer() = default;
 
+  using LinkListNode = std::shared_ptr<T>;
+  using LinkList = std::list<std::shared_ptr<T>>;
+using LinkListNodePtr = typename LinkList::iterator;
+
+
 public:
   bool updateVerificationStatus(const QString &unique_id,
                                 const QString &msg_id) {
 
-    if (!m_localMsgIndex.count(unique_id)) {
-      qDebug() << "Unique ID Not Found!";
-      return false;
-    }
+      if (msg_id.isEmpty() || unique_id.isEmpty()) {
+          qDebug() << "Empty msg_id or unique_id!";
+          return false;
+      }
 
     auto it = m_localMsgIndex.find(unique_id);
-    std::shared_ptr<T> msg = *(it->second);
+    if(it == m_localMsgIndex.end()){
+        qDebug() << "Unique ID Not Found!";
+        return false;
+    }
+
+    if (!m_verifyMessage.count(msg_id)) {
+        qDebug() << "Msg ID already exists in verified messages!";
+        return false;
+    }
+
+    auto node_it = it->second;
+    auto& msg    = *node_it;
+    if (!msg) {
+        qDebug() << "Null message pointer!";
+        return false;
+    }
+
     msg->setMsgID(msg_id);
 
-    return insertVerified(msg) && removeLocal(unique_id);
+    /* <msg_id, pointer> */
+    auto [_, inserted] = m_verifyMessage.try_emplace(msg_id, node_it);
+    if (!inserted) {
+        qDebug() << "Msg ID already exists in verified messages!";
+        return false;
+    }
+
+    //erase the local iterator pointer from the local unordered_map
+    m_localMsgIndex.erase(it);
+    return true;
   }
 
   [[nodiscard]]
   std::vector<std::shared_ptr<T>> dumpAllChatData() const {
-    std::vector<std::shared_ptr<T>> ret;
-    ret.reserve(m_verifyMessage.size() + m_localMessage.size());
-
-    for (const auto &pair : m_verifyMessage)
-      ret.push_back(pair.second);
-
-    for (const auto &msg : m_localMessage)
-      ret.push_back(msg);
-
-    return ret;
+    return {m_messages.begin(), m_messages.end()};
   }
 
-  bool insertMessage(std::shared_ptr<T> type) { return _insert(type); }
+  bool insertMessage(std::shared_ptr<T> type) {
+      return _insert(type);
+  }
 
   bool _insert(std::shared_ptr<T> value) {
 
-    if (value->isOnLocal()) {
-      return insertLocal(value);
-    }
-    return insertVerified(value);
-  }
+      //put it inside linklist directly to ensure the sequences
+    auto it = m_messages.insert(m_messages.end(), value);
 
-  [[nodiscard]]
-  std::vector<std::shared_ptr<T>> getLocalMessages() const {
-    return {m_localMessage.begin(), m_localMessage.end()};
+      bool ok = value->isOnLocal()
+                    ? insertLocal(value->getUniqueId(), it)
+                    : insertVerified(value, it);
+
+      //Rollback to ensure safty
+      if (!ok) {
+          qDebug() << "Rollback Happening!\n";
+          m_messages.erase(it);
+      }
+      return ok;
   }
 
   void clear() {
-    m_localMessage.clear();
+    m_messages.clear();
     m_localMsgIndex.clear();
     m_verifyMessage.clear();
     m_lastFetchedMsgId.clear();
   }
 
 private:
-  bool insertLocal(std::shared_ptr<T> msg) {
-    const QString &id = msg->getUniqueId();
-    if (m_localMsgIndex.count(id))
+
+  bool insertLocal(const QString &unique_id,  LinkListNodePtr pointer) {
+
+      if (unique_id.isEmpty()) {
+          qDebug() << "Empty munique_id!";
+          return false;
+      }
+
+    if (m_localMsgIndex.count(unique_id))
       return false;
 
-    auto it = m_localMessage.insert(m_localMessage.end(), msg);
-    m_localMsgIndex[id] = it;
+    /* <unique_id, pointer> */
+    auto [it, inserted] = m_localMsgIndex.try_emplace(unique_id, pointer);
+    if (!inserted) {
+        qDebug() << "Unique Id already exists in local messages!";
+        return false;
+    }
     return true;
   }
 
-  bool removeLocal(const QString &id) {
-    if (!m_localMsgIndex.count(id))
-      return false;
-
-    auto it = m_localMsgIndex.find(id);
-    m_localMessage.erase(it->second);
-    m_localMsgIndex.erase(it);
-    return true;
-  }
-
-  bool insertVerified(std::shared_ptr<T> value) {
+  bool insertVerified(std::shared_ptr<T> value, LinkListNodePtr pointer) {
     if (!value) {
       qDebug() << "Null value!";
       return false;
@@ -103,14 +130,8 @@ private:
       return false;
     }
 
-    bool converted = false;
-    const qint64 msgid = idStr.toLongLong(&converted, 10);
-    if (!converted || msgid < 0) {
-      qDebug() << "Invalid msg_id! It should be a positive integer!";
-      return false;
-    }
-
-    auto [it, inserted] = m_verifyMessage.try_emplace(msgid, std::move(value));
+    /* <msg_id, pointer> */
+    auto [it, inserted] = m_verifyMessage.try_emplace(idStr, pointer);
     if (!inserted) {
       qDebug() << "Msg ID already exists in verified messages!";
       return false;
@@ -121,16 +142,27 @@ private:
   }
 
 private:
-  // Messages which only exist on local!
-  std::list<std::shared_ptr<T>> m_localMessage;
 
-  std::unordered_map<QString, typename decltype(m_localMessage)::iterator>
+  // Messages which are stored in here!
+  // local message iterator(pointer) is going to store in unordered_map
+  // remote message iterator(pointer) is going to store in map
+  LinkList m_messages;
+
+  std::unordered_map<
+      /*unique_id*/
+      QString,
+      /*the pointer to msg*/
+      LinkListNodePtr
+      >
       m_localMsgIndex;
 
   // Messages which are verified by the server!
   std::map<
-      /*msg_id*/ qint64,
-      /*msg_data*/ std::shared_ptr<T>>
+      /*msg_id*/
+      QString,
+      /*the pointer to msg*/
+      LinkListNodePtr
+      >
       m_verifyMessage;
 
   QString m_lastFetchedMsgId;

89e42aa45828013fea9a941a3bc646e4328d1e78
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-09-07 12:00:33 +1000

    1.[MOVE]:move Dispatcher src code to header file 2.[FIX]:fix wrong log

--- resources-server/include/dispatcher/FileProcessingDispatcher.hpp
+++ resources-server/include/dispatcher/FileProcessingDispatcher.hpp
@@ -1,14 +1,15 @@
 #pragma once
 #ifndef _FILE_PROCESSING_DISPATCHER_HPP_
 #define _FILE_PROCESSING_DISPATCHER_HPP_
-#include <handler/FileProcessingNode.hpp>
 #include <memory>
+#include <algorithm>
+#include <spdlog/spdlog.h>
+#include <server/Session.hpp>
 #include <singleton/singleton.hpp>
 #include <tbb/concurrent_vector.h>
-
-namespace handler {
-class FileDescriptionBlock;
-}
+#include <config/ServerConfig.hpp>
+#include <server/FileHasherLogger.hpp>
+#include <handler/FileProcessingNode.hpp>
 
 namespace dispatcher {
 
@@ -20,30 +21,96 @@ class FileProcessingDispatcher : public Singleton<FileProcessingDispatcher> {
   using ContainerType = tbb::concurrent_vector<FPTRType>;
 
 public:
-  virtual ~FileProcessingDispatcher();
-  void shutdown();
+          virtual ~FileProcessingDispatcher() {
+                    shutdown();
+          }
+          void shutdown() {
+                    std::for_each(m_nodes.begin(), m_nodes.end(),
+                              [](std::shared_ptr<handler::FileProcessingNode>& node) {
+                                        node->shutdown();
+                              });
+          }
 
   void commit(std::unique_ptr<handler::FileDescriptionBlock> block,
-              [[maybe_unused]] SessionPtr live_extend);
+            [[maybe_unused]] SessionPtr live_extend) {
+
+            if (!block) return;
+            auto filename = block->filename;
+
+            auto temp = dispatch_to_node(filename);
+            if (!temp.has_value()) {
+                      spdlog::error("[{}]: Dispatch Error While Handling Session: {}",
+                                ServerConfig::get_instance()->GrpcServerName,
+                                filename);
+                      return;
+            }
+
+            auto node = temp.value();
+            node->commit(std::move(block), live_extend);
+
+            //spdlog::info("[{}]: Dispatch Task To Node {} Successfully!",
+            //          ServerConfig::get_instance()->GrpcServerName,
+            //          node->getProcessingId());
+  }
 
   void commit(const std::string &filename, const std::string &block_data,
               const std::string &checksum, const std::string &curr_sequence,
               const std::string &last_sequence, const std::string &_eof,
               std::size_t accumlated_size, std::size_t file_size,
-              [[maybe_unused]] SessionPtr live_extend);
+            [[maybe_unused]] SessionPtr live_extend) {
+
+            commit(std::make_unique<handler::FileDescriptionBlock>(
+                      filename, block_data, checksum, curr_sequence, last_sequence, _eof,
+                      accumlated_size, file_size),
+                      live_extend);
+  }
 
 protected:
-  const std::size_t hash_to_index(std::string_view filename) const;
+          const std::size_t hash_to_index(std::string_view filename) const {
+                    return m_convertor(filename) % m_nodes.size();
+          }
 
 private:
-  FileProcessingDispatcher();
-  FileProcessingDispatcher(std::size_t threads);
+          FileProcessingDispatcher()
+                    : FileProcessingDispatcher(std::thread::hardware_concurrency() < 2
+                              ? 2
+                              : std::thread::hardware_concurrency()) {
+          }
+          FileProcessingDispatcher(std::size_t threads)
+                    : m_convertor() {
+                    for (std::size_t thread = 0; thread < threads; ++thread) {
+                              m_nodes.emplace_back(std::make_shared<handler::FileProcessingNode>(thread));
+                    }
+          }
 
   [[nodiscard]] typename ContainerType::iterator
-  dispatch_to_iterator(std::string_view filename);
+            dispatch_to_iterator(std::string_view filename) {
+            if (m_nodes.empty()) {
+                      return m_nodes.end(); // or throw
+            }
+
+            auto it = m_nodes.begin() + hash_to_index(filename);
+            if (it != m_nodes.end()) {
+                      return it;
+            }
+            return m_nodes.end();
+  }
 
   [[nodiscard]] std::optional<FPTRType>
-  dispatch_to_node(std::string_view filename);
+      dispatch_to_node(std::string_view filename) {
+      if (m_nodes.empty())
+          return std::nullopt;
+
+      try {
+          return m_nodes.at(hash_to_index(filename));
+      }
+      catch (const std::exception& e) {
+          spdlog::error("[{}]: Find File Processing Node Err, Reason:{}",
+              ServerConfig::get_instance()->GrpcServerName,
+              e.what());
+      }
+      return std::nullopt;
+  }
 
 private:
   std::hash<std::string_view> m_convertor;
--- resources-server/include/dispatcher/RequestHandlerDispatcher.hpp
+++ resources-server/include/dispatcher/RequestHandlerDispatcher.hpp
@@ -1,13 +1,16 @@
 #pragma once
 #ifndef _REQUEST_HANDLER_DISPATCHER_HPP_
 #define _REQUEST_HANDLER_DISPATCHER_HPP_
-#include <buffer/MsgNode.hpp>
-#include <handler/RequestHandlerNode.hpp>
-#include <optional>
-#include <singleton/singleton.hpp>
 #include <string>
+#include <optional>
 #include <string_view>
+#include <spdlog/spdlog.h>
+#include <server/Session.hpp>
+#include <buffer/MsgNode.hpp>
+#include <singleton/singleton.hpp>
 #include <tbb/concurrent_vector.h>
+#include <config/ServerConfig.hpp>
+#include <handler/RequestHandlerNode.hpp>
 
 namespace dispatcher {
 
@@ -21,19 +24,81 @@ class RequestHandlerDispatcher : public Singleton<RequestHandlerDispatcher> {
       tbb::concurrent_vector<std::shared_ptr<handler::RequestHandlerNode>>;
 
 public:
-  virtual ~RequestHandlerDispatcher();
-  void commit(pair &&recv_node, [[maybe_unused]] SessionPtr live_extend);
+          virtual ~RequestHandlerDispatcher() {
+                    shutdown();
+          }
+  void commit(pair&& recv_node, [[maybe_unused]] SessionPtr live_extend) {
+
+            if (!recv_node.first) return;
+            auto session_id = recv_node.first->get_session_id();
+
+            // if opt has value then it could be executed by this if condition
+            auto temp = dispatch_to_node(session_id);
+            if (!temp.has_value()) {
+                      spdlog::error("[{}]: Dispatcher Error While Handling Session: {}",
+                                ServerConfig::get_instance()->GrpcServerName,
+                                session_id);
+                      return;
+            }
+
+            auto node = temp.value();
+            node->commit(std::move(recv_node), live_extend);
+
+            //spdlog::info("[{}]: Dispatcher Request To Handler Node {} Successfully!",
+            //          ServerConfig::get_instance()->GrpcServerName,
+            //          node->getId());
+  }
 
 protected:
-  std::size_t hash_to_index(std::string_view session_id) const;
+          std::size_t hash_to_index(std::string_view session_id) const {
+                    return m_convertor(session_id) % m_nodes.size();
+          }
 
 private:
-  RequestHandlerDispatcher();
-  RequestHandlerDispatcher(std::size_t threads);
-  void shutdown();
-  ContainerType::iterator dispatch_to_iterator(std::string_view session_id);
+          RequestHandlerDispatcher()
+                    : RequestHandlerDispatcher(std::thread::hardware_concurrency() < 2
+                              ? 2
+                              : std::thread::hardware_concurrency()) {
+          }
+          RequestHandlerDispatcher(std::size_t threads)
+                    : m_convertor() {
+                    for (std::size_t thread = 0; thread < threads; ++thread) {
+                              m_nodes.emplace_back(std::make_shared<handler::RequestHandlerNode>(thread));
+                    }
+          }
+          void shutdown() {
+                    std::for_each(m_nodes.begin(), m_nodes.end(),
+                              [](std::shared_ptr<handler::RequestHandlerNode>& node) {
+                                        node->shutdown();
+                              });
+          }
+  ContainerType::iterator dispatch_to_iterator(std::string_view session_id) {
+            if (m_nodes.empty()) {
+                      return m_nodes.end(); // or throw
+            }
+
+            auto it = m_nodes.begin() + hash_to_index(session_id);
+            if (it != m_nodes.end()) {
+                      return it;
+            }
+            return m_nodes.end();
+  }
   std::optional<std::shared_ptr<handler::RequestHandlerNode>>
-  dispatch_to_node(std::string_view session_id);
+      dispatch_to_node(std::string_view session_id) {
+      if (m_nodes.empty())
+          return std::nullopt;
+
+      try {
+          return m_nodes.at(hash_to_index(session_id));
+      }
+      catch (const std::exception& e) {
+          spdlog::error(
+              "[{}]: Retrieve Request Handler Node Error, Reason:{}",
+                    ServerConfig::get_instance()->GrpcServerName,
+              e.what());
+      }
+      return std::nullopt;
+  }
 
 private:
   std::hash<std::string_view> m_convertor;

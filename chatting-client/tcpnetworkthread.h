143d360ef726a55a0bd4db6704405ee888956191
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-09-07 19:51:57 +1000

    1.[UPDATE]:now support two network threads to handle resources and chatting service

--- chatting-client/tcpnetworkthread.h
+++ chatting-client/tcpnetworkthread.h
@@ -3,33 +3,45 @@
 #include <QObject>
 #include <QThread>
 #include <memory>
-#include <tcpnetworkconnection.h>
+#include <chattingtcpnetwork.h>
+#include <filetcpnetwork.h>
 
-class TCPNetworkThread : public QObject,
-                         public std::enable_shared_from_this<TCPNetworkThread> {
+class TCPNetworkThread : public QObject {
   Q_OBJECT
 
 public:
   TCPNetworkThread(QObject *parent = nullptr)
-      : QObject{parent},
-        m_thread(new QThread()) { // you should NOT use parent at here!
+      : QObject{parent}
+      ,chatting_thread(new QThread())  // you should NOT use parent at here!
+    , resource_thread(new QThread())  // you should NOT use parent at here!
+{
 
     // now all tcpnetwork class are moving to this thread!
-    TCPNetworkConnection::get_instance()->moveToThread(m_thread);
+    ChattingTCPNetwork::get_instance()->moveToThread(chatting_thread);
+
+    // now all tcpnetwork class are moving to this thread!
+    FileTCPNetwork::get_instance()->moveToThread(resource_thread);
 
     registerSignal();
-    m_thread->start();
-  }
 
-  virtual ~TCPNetworkThread() { m_thread->quit(); }
+    chatting_thread->start();
+    resource_thread->start();
+}
+
+virtual ~TCPNetworkThread() {
+      chatting_thread->quit();
+      resource_thread->quit();
+  }
 
 private:
   void registerSignal() {
-    connect(m_thread, &QThread::finished, m_thread, &QObject::deleteLater);
+    connect(chatting_thread, &QThread::finished, chatting_thread, &QObject::deleteLater);
+    connect(resource_thread, &QThread::finished, resource_thread, &QObject::deleteLater);
   }
 
 private:
-  QThread *m_thread;
+  QThread *chatting_thread;
+  QThread *resource_thread;
 };
 
 #endif // TCPNETWORKTHREAD_H

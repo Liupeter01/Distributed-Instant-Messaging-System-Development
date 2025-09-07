d2bf77755a94c78cbd81ac2d25fa10cf612d5402
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-09-07 22:34:33 +1000

    1.[ADD]:add local checksum data storge data structure, because this class is singleton 2.[ADD]:delegate signal to logicexecutor 3.[ADD]:add m_thread to execute one LogicExecutor

--- chatting-client/logicmethod.cpp
+++ chatting-client/logicmethod.cpp
@@ -1,9 +1,13 @@
 #include "logicmethod.h"
-#include <tcpnetworkconnection.h>
+#include <filetcpnetwork.h>
 
 LogicMethod::LogicMethod(QObject *parent)
-    : QObject{parent}, m_thread(new QThread(this)),
+    : QObject{parent}, m_thread(new QThread()),
       m_exec(new LogicExecutor()) {
+
+  //meta type
+  registerMetaType();
+
   /*move executor to subthread*/
   m_exec->moveToThread(m_thread);
 
@@ -14,11 +18,8 @@ LogicMethod::LogicMethod(QObject *parent)
   m_thread->start();
 }
 
-LogicMethod::~LogicMethod() {
-  m_thread->quit();
-  m_thread->wait();
-  m_thread->deleteLater();
-  m_exec->deleteLater();
+void LogicMethod::registerMetaType() {
+    qRegisterMetaType<std::shared_ptr<QFileInfo>>("std::shared_ptr<QFileInfo>");
 }
 
 void LogicMethod::registerSignals() {
@@ -32,4 +33,50 @@ void LogicMethod::registerSignals() {
    */
   connect(m_exec, &LogicExecutor::signal_data_transmission_status, this,
           &LogicMethod::signal_data_transmission_status);
+
+  connect(this, &LogicMethod::signal_start_file_transmission, m_exec,
+          &LogicExecutor::signal_start_file_transmission);
+
+  connect(this, &LogicMethod::signal_pause_file_transmission, m_exec,
+          &LogicExecutor::signal_pause_file_transmission);
+
+  connect(this, &LogicMethod::signal_resume_file_transmission, m_exec,
+          &LogicExecutor::signal_resume_file_transmission);
+
+  connect(m_thread, &QThread::finished, m_thread, &QObject::deleteLater);
 }
+
+void LogicMethod::recordMD5Progress(const QString &md5,
+                                    std::shared_ptr<QFileInfo> info){
+
+    std::lock_guard<std::mutex> _lckg(m_mtx);
+
+    auto it = m_md5_cache.find(md5);
+
+    //only update
+    if(it != m_md5_cache.end()){
+        if(it->second){
+            it->second.reset();
+        }
+        it->second = info;
+        return;
+    }
+    auto [_, status] = m_md5_cache.try_emplace(md5, info);
+    if(!status){
+        qDebug() << "md5 uuid exist!\n";
+    }
+}
+
+std::optional<std::shared_ptr<QFileInfo>>
+LogicMethod::getFileByMD5(const QString &md5){
+
+    std::lock_guard<std::mutex> _lckg(m_mtx);
+    auto it = m_md5_cache.find(md5);
+    if(it == m_md5_cache.end()){
+        return std::nullopt;
+    }
+    return it->second;
+}
+
+bool LogicMethod::getPauseStatus() const { return m_pause; }
+void LogicMethod::setPause(const bool status) { m_pause = status; }
--- chatting-client/logicmethod.h
+++ chatting-client/logicmethod.h
@@ -1,27 +1,57 @@
+#pragma once
 #ifndef LOGICMETHOD_H
 #define LOGICMETHOD_H
 
 #include <QObject>
 #include <QThread>
 #include <singleton.hpp>
-/*execute some specfic logic from network*/
-#include <logicexecutor.h>
+#include <logicexecutor.h>  /*execute some specfic logic from network*/
+#include <optional>
+#include <atomic>
+#include <unordered_map>
+#include <QFileInfo>
 
-class LogicMethod : public QObject, public Singleton<LogicMethod> {
+class LogicMethod
+    : public QObject
+    , public Singleton<LogicMethod> {
 
   Q_OBJECT
   friend class Singleton<LogicMethod>;
 
 public:
-  virtual ~LogicMethod();
+  virtual ~LogicMethod() {
+      m_thread->quit();
+  }
+
+public:
+  bool getPauseStatus() const;
+  void setPause(const bool status);
+  void recordMD5Progress(const QString& md5,
+                         std::shared_ptr<QFileInfo> info);
+
+  [[nodiscard]]
+  std::optional<std::shared_ptr<QFileInfo>>
+  getFileByMD5(const QString& md5);
 
 private:
   explicit LogicMethod(QObject *parent = nullptr);
   void registerSignals();
+  void registerMetaType();
 
 signals:
   /*forward resources server's message to a standlone logic thread*/
   void signal_resources_logic_handler(const uint16_t id, const QJsonObject obj);
+
+  // start transmission(with init filename & filepath)
+    void signal_start_file_transmission(const QString &fileName,
+                                        const QString &filePath,
+                                        const std::size_t fileChunk = 4096);
+
+    //pause transmission
+    void signal_pause_file_transmission();
+
+    //resume transmission
+    void signal_resume_file_transmission();
 
   /*data transmission status*/
   void signal_data_transmission_status(const QString &filename,
@@ -33,6 +63,18 @@ signals:
 private:
   QThread *m_thread;
   LogicExecutor *m_exec;
+
+  //transmission stopped
+  std::atomic<bool> m_pause = false;
+
+  //mutex
+  std::mutex m_mtx;
+
+  //store info in mapping structure
+  std::unordered_map<
+      /*    md5 = */ QString,
+      /*QFileInfo=*/ std::shared_ptr<QFileInfo>>
+      m_md5_cache;
 };
 
 #endif // LOGICMETHOD_H

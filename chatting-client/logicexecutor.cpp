ac10164e98bd9279ec87bf1664672baa4dea8ab7
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2026-04-04 21:30:41 +1100

    1.[REMOVE]:remove filetransferthread 2.[ADD]:wee add executor to become one node inside locigmethod 3.[FEATURE]:this model with control the start, pause, resume status of the file transmission and all the block transfer are driven by the send_next_block signal which is actually triggered by the server response

--- chatting-client/logicexecutor.cpp
+++ chatting-client/logicexecutor.cpp
@@ -1,46 +1,227 @@
-#include "logicexecutor.h"
 #include <QDebug>
+#include <QByteArray>
 #include <QJsonDocument>
+#include <logicmethod.h>
+#include "logicexecutor.h"
+#include <QCryptographicHash>
+#include <filetcpnetwork.h>
 
-LogicExecutor::LogicExecutor() { registerCallbacks(); }
+[[nodiscard]]
+std::size_t
+LogicExecutor::calculateBlockNumber(const std::size_t totalSize,
+                                        const std::size_t chunkSize){
+
+    return static_cast<size_t>(
+        std::ceil(static_cast<double>(totalSize) / chunkSize));
+}
+
+LogicExecutor::LogicExecutor(QObject *parent)
+    :QObject(parent){
+
+    //register callback
+    registerCallbacks();
+
+    //register signal
+    registerSignal();
+}
 
 LogicExecutor::~LogicExecutor() {}
 
+void LogicExecutor::registerSignal(){
+    connect(this, &LogicExecutor::signal_start_file_transmission, this,
+            &LogicExecutor::slot_start_file_transmission);
+
+    connect(this, &LogicExecutor::signal_send_next_block, this,
+            &LogicExecutor::slot_send_next_block);
+
+    connect(this, &LogicExecutor::signal_pause_file_transmission, this,
+            &LogicExecutor::slot_pause_file_transmission);
+
+    connect(this, &LogicExecutor::signal_resume_file_transmission, this,
+            &LogicExecutor::slot_resume_file_transmission);
+
+    /*
+   * flow control Server will return transmission status back(EOF or not?)
+   * If NOT eof, this transmission progress could be paused!
+   */
+    connect(this, &LogicExecutor::signal_data_transmission_status, this,
+            [this](const QString &checksum, const std::size_t curr_seq,
+                   const std::size_t curr_size, const std::size_t total_size,
+                   const bool eof) {
+
+                if(eof) {
+                    m_curSeq = 0;
+                    accumulate_transferred = 0;
+
+                    //reset pause status to prevent unexpected error!
+                    LogicMethod::get_instance()->setPause(false);
+                }
+                else{
+                    m_curSeq = curr_seq + 1;
+                    accumulate_transferred = curr_size;
+                    emit signal_send_next_block(checksum);
+                }
+            });
+}
+
 void LogicExecutor::registerCallbacks() {
 
-  m_callbacks.insert(std::pair<ServiceType, Callbackfunction>(
-      ServiceType::SERVICE_FILEUPLOADRESPONSE, [this](const QJsonObject json) {
+    auto updateNextBlocksInfo = [this](const QJsonObject json){
         /*error occured!*/
         if (!json.contains("error")) {
-          qDebug() << "Json Parse Error!";
-          return;
+            qDebug() << "Json Parse Error!";
+            return;
         }
         if (json["error"].toInt() !=
             static_cast<int>(ServiceStatus::SERVICE_SUCCESS)) {
-          qDebug() << "Login Server Error!";
-          return;
+            qDebug() << "File Transmission Resume Failed!";
+            return;
         }
 
-        [[maybe_unused]] auto filename = json["filename"].toString();
+        [[maybe_unused]] auto checksum = json["checksum"].toString();
         [[maybe_unused]] auto curr_seq = json["curr_seq"].toString();
         [[maybe_unused]] auto curr_size = json["curr_size"].toString();
         [[maybe_unused]] auto total_size = json["total_size"].toString();
         [[maybe_unused]] auto eof = json["EOF"].toBool();
 
-        /*notifying the main UI interface to update progress bar!*/
-        emit signal_data_transmission_status(filename, curr_seq.toUInt(),
+        // /*notifying the main UI interface to update progress bar!*/
+        emit signal_data_transmission_status(checksum, curr_seq.toUInt(),
                                              curr_size.toUInt(),
                                              total_size.toUInt(), eof);
-      }));
+    };
+
+    m_callbacks.insert(std::pair<ServiceType, Callbackfunction>(
+        ServiceType::SERVICE_FILEUPLOADRESPONSE, updateNextBlocksInfo));
+
+    m_callbacks.insert(std::pair<ServiceType, Callbackfunction>(
+        ServiceType::SERVICE_FILECHECKUPLOADPROGRESSRESPONSE, updateNextBlocksInfo));
 }
 
 void LogicExecutor::slot_resources_logic_handler(const uint16_t id,
                                                  const QJsonObject obj) {
-
   try {
     m_callbacks[static_cast<ServiceType>(id)](obj);
-
   } catch (const std::exception &e) {
     qDebug() << e.what();
   }
+}
+
+void LogicExecutor::slot_send_next_block(const QString& checksum){
+
+    // if(m_fileCheckSum != checksum){
+    //     //TODO
+    // }
+    auto temp = LogicMethod::get_instance()->getFileByMD5(checksum);
+    if(!temp.has_value()){
+        qDebug() << "File Not Found! Please Open a new one!\n";
+        return;
+    }
+
+    //update checksum info
+    m_fileCheckSum = checksum;
+
+    //open file
+    QFile file(temp.value()->filePath());
+
+    if(!file.isOpen()){
+        if (!file.open(QIODevice::ReadOnly)) {
+            qDebug() << "Cannot Use ReadOnly Mode To Open File";
+            return;
+        }
+    }
+
+    //this is the current file pointer startup point!
+    file.seek(accumulate_transferred);
+
+    //Did user click paused?
+    if(LogicMethod::get_instance()->getPauseStatus()){
+        return;
+    }
+
+    if(file.atEnd()){
+        qDebug() << "Rearch At the End of the File, Terminate Process!";
+        return;
+    }
+
+    std::size_t bytes_transferred_curr_sequence =
+        (m_curSeq != m_totalBlocks) ? m_fileChunk
+                                    : m_fileSize - (m_curSeq - 1) * m_fileChunk;
+
+    QByteArray buffer = file.read(bytes_transferred_curr_sequence);
+    if (buffer.isEmpty()) {
+        qDebug() << "transferred bytes = 0 in seq = " << m_curSeq;
+        return;
+    }
+
+    QJsonObject obj;
+    obj["filename"] = m_fileName;
+    obj["checksum"] = m_fileCheckSum;
+    obj["cur_seq"] = QString::number(m_curSeq);
+    obj["last_seq"] = QString::number(m_totalBlocks);
+    obj["cur_size"] = QString::number(accumulate_transferred + bytes_transferred_curr_sequence);
+    obj["file_size"] = QString::number(m_fileSize);
+    obj["block"] = QString(buffer.toBase64());
+    obj["EOF"] = (m_curSeq == m_totalBlocks) ? "1" : "0";
+
+    FileTCPNetwork::get_instance()->send_buffer(ServiceType::SERVICE_FILEUPLOADREQUEST,
+                                           std::move(obj));
+
+    file.close();
+}
+
+void LogicExecutor::slot_start_file_transmission(const QString &fileName, const QString &filePath, const std::size_t fileChunk)
+{
+    m_fileName = fileName;
+    m_filePath = filePath;
+    m_fileChunk = fileChunk;
+    m_curSeq = 1;
+
+    QFile file(filePath);
+    if(!file.isOpen()){
+        if (!file.open(QIODevice::ReadOnly)) {
+            qDebug() << "Cannot Use ReadOnly Mode To Open File";
+            return;
+        }
+    }
+
+    m_fileSize = file.size();
+    m_totalBlocks = calculateBlockNumber(m_fileSize, m_fileChunk);
+
+    /*use md5 to mark this file*/
+    QCryptographicHash hash(QCryptographicHash::Md5);
+
+    /*
+   * try to hash the whole file and generate a unique record
+   * WARNING: after call addData, the file pointer will move to
+   * another position rather than current pos
+   */
+    if (!hash.addData(&file)) {
+        qDebug() << "Hashing File Failed!";
+        return;
+    }
+
+    m_fileCheckSum = QString::fromStdString(hash.result().toHex().toStdString());
+    file.seek(0);
+
+    std::shared_ptr<QFileInfo> info = std::make_shared<QFileInfo>(file);
+    LogicMethod::get_instance()->recordMD5Progress(/* QString */m_fileCheckSum,
+                                                   /* std::shared_ptr<QFileInfo> */info);
+
+    emit signal_send_next_block(m_fileCheckSum);
+
+    file.close();
+}
+
+void LogicExecutor::slot_pause_file_transmission(){
+    LogicMethod::get_instance()->setPause(true);
+}
+
+void LogicExecutor::slot_resume_file_transmission(){
+    LogicMethod::get_instance()->setPause(false);
+
+    QJsonObject obj;
+    obj["checksum"] = m_fileCheckSum;
+
+    FileTCPNetwork::get_instance()->send_buffer(ServiceType::SERVICE_FILECHECKUPLOADPROGRESSREQUEST,
+                                           std::move(obj));
 }
--- chatting-client/logicexecutor.h
+++ chatting-client/logicexecutor.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef LOGICEXECUTOR_H
 #define LOGICEXECUTOR_H
 
@@ -5,6 +6,8 @@
 #include <QObject>
 #include <def.hpp>
 #include <unordered_map>
+#include <QFile>
+#include <MsgNode.hpp>
 
 class LogicMethod;
 
@@ -12,29 +15,77 @@ class LogicExecutor : public QObject {
   Q_OBJECT
   friend class LogicMethod;
   using Callbackfunction = std::function<void(const QJsonObject)>;
+  using SendNodeType = SendNode<QByteArray, ByteOrderConverterReverse>;
 
 public:
-  LogicExecutor();
+  explicit LogicExecutor(QObject *parent = nullptr);
   virtual ~LogicExecutor();
 
-private:
-  void registerCallbacks();
+public:
+  [[nodiscard]]
+  static
+  std::size_t calculateBlockNumber(const std::size_t totalSize,
+                                             const std::size_t chunkSize);
 
 signals:
+
+    void signal_start_file_transmission(const QString &fileName,
+                                        const QString &filePath,
+                                        const std::size_t fileChunk);
+
+  //pause transmission
+  void signal_pause_file_transmission();
+
+  //resume transmission
+  void signal_resume_file_transmission();
+
+  void signal_send_next_block(const QString& checksum);
+
   /*data transmission status*/
-  void signal_data_transmission_status(const QString &filename,
+  void signal_data_transmission_status(const QString &checksum,
                                        const std::size_t curr_seq,
                                        const std::size_t curr_size,
                                        const std::size_t total_size,
                                        const bool eof);
 
+  private:
+      void registerSignal();
+      void registerCallbacks();
+
 private slots:
   /*forward resources server's message to a standlone logic thread*/
   void slot_resources_logic_handler(const uint16_t id, const QJsonObject obj);
 
+    void slot_send_next_block(const QString& checksum);
+    void slot_start_file_transmission(const QString &fileName,
+                                      const QString &filePath,
+                                      const std::size_t fileChunk);
+
+
+  //pause transmission
+    void slot_pause_file_transmission();
+
+    //resume transmission
+    void slot_resume_file_transmission();
+
 private:
+  QString m_fileName;
+  QString m_filePath;
+  QString m_fileCheckSum;
+
+  std::size_t m_fileSize = 0;
+  std::size_t m_fileChunk = 0;
+  std::size_t m_curSeq = 1;
+  std::size_t m_totalBlocks = 0;
+
+  // accumulate transferred size(from seq = 1 to n)
+  std::size_t accumulate_transferred{0};
+
+  // transfered size for a single seq(maybe seq =1, or seq = 2)
+  std::size_t bytes_transferred_curr_sequence{0};
+
   /*according to service type to execute callback*/
-  std::map<ServiceType, Callbackfunction> m_callbacks;
+  std::unordered_map<ServiceType, Callbackfunction> m_callbacks;
 };
 
 #endif // LOGICEXECUTOR_H

98f61bf9fada31b8a22a9e69c2d177d6cfc01f8d
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2026-04-09 11:37:03 +1000

    1.[UPDATE]: FileHasherDesc & FileDescriptionBlock could be utilized by both download & upload process 2.[ADD]:add functor inside  FileDescriptionBlock to async callback

--- resources-server/include/server/FileHasherLogger.hpp
+++ resources-server/include/server/FileHasherLogger.hpp
@@ -4,21 +4,30 @@
 #include <optional>
 #include <singleton/singleton.hpp>
 #include <string>
-#include <tbb/concurrent_hash_map.h>
+#include <boost/json.hpp>
+#include <boost/json/object.hpp>
+#include <boost/json/parse.hpp>
+#include <functional>
+#include <network/def.hpp>
+
+/* 
+ * we no longer gonna to leave it at server's memory (tbb/concurrent_hash_map)
+ * we will choose redis instead!
+*/
+#include <redis/RedisManager.hpp>
 
 namespace handler {
 
 struct FileHasherDesc {
-  FileHasherDesc(const std::string &_filename, const std::string &_checksum,
-                 const std::string &_curr_sequence,
-                 const std::string &_last_sequence, const std::string &_eof,
-                 std::size_t _transfered_size, std::size_t  _current_block_size, std::size_t _total_size)
-      : filename(_filename), checksum(_checksum), curr_sequence(_curr_sequence),
-        last_sequence(_last_sequence), transfered_size(_transfered_size),
-            current_block_size(_current_block_size),
-            total_size(_total_size), isEOF(_eof) {}
+          FileHasherDesc(const std::string& _filename, const std::string& _checksum, const std::string& _filePath,
+                    const std::string& _curr_sequence,
+                    const std::string& _last_sequence, const std::string& _eof,
+                    std::size_t _transfered_size, std::size_t  _current_block_size, std::size_t _total_size);
+
+  std::string key; //=filename_checksum
 
   std::string filename;
+  std::string filePath;
   std::string checksum;
   std::string curr_sequence;
   std::string last_sequence;
@@ -29,81 +38,81 @@ struct FileHasherDesc {
   std::size_t total_size;
 };
 
-struct FileDescriptionBlock {
-  FileDescriptionBlock() = default;
-  FileDescriptionBlock(const FileHasherDesc& o, const std::string& block)
-            : FileDescriptionBlock(o.filename, block, o.checksum, o.curr_sequence,
-                      o.last_sequence, o.isEOF, o.transfered_size,
-                      o.current_block_size, o.total_size) {
-  }
+struct FileDescriptionBlock : public FileHasherDesc {
+          FileDescriptionBlock(const FileHasherDesc& o,
+                    const std::string& block,
+                    std::function<void(const ServiceStatus)>&& _callback);
 
-  FileDescriptionBlock(const std::string& _filename,
-            const std::string& _block_data,
-            const std::string& _checksum,
-            const std::string& _curr_sequence,
-            const std::string& _last_sequence,
-            const std::string& _eof,
-            std::size_t _transfered_size,
-            std::size_t  _current_block_size,
-            std::size_t _total_size)
-
-            : filename(_filename), block_data(_block_data), checksum(_checksum),
-            curr_sequence(_curr_sequence), last_sequence(_last_sequence),
-            transfered_size(_transfered_size),
-            current_block_size(_current_block_size),
-            total_size(_total_size), isEOF(_eof) {
-  }
-
-  std::string filename;
   std::string block_data;
-  std::string checksum;
-  std::string curr_sequence;
-  std::string last_sequence;
-  std::string isEOF;
-
-  std::size_t transfered_size;
-  std::size_t current_block_size;
-  std::size_t total_size;
+  std::function<void(const ServiceStatus)> callback;
 };
 } // namespace handler
 
 class FileHasherLogger : public Singleton<FileHasherLogger> {
   friend class Singleton<FileHasherLogger>;
   friend class AsyncServer;
-  using ContainerType = tbb::concurrent_hash_map<
-      /*checksum data*/ std::string,
-      /*file description block*/ std::shared_ptr<
-          const handler::FileHasherDesc>>;
-
   FileHasherLogger() = default;
+
+  using RedisRAII = connection::ConnectionRAII<redis::RedisConnectionPool,
+            redis::RedisContext>;
 
 public:
   ~FileHasherLogger() = default;
 
 public:
-  void insert(const std::string &checksum,
-              std::shared_ptr<const handler::FileHasherDesc> block) {
-    typename ContainerType::accessor accessor;
-    if (m_checksum2FileDesc.find(accessor, checksum)) {
-      accessor->second.reset();
-      accessor->second = block; // update!
-      return;
-    }
+          bool insert(std::shared_ptr<const handler::FileHasherDesc> block) {
+                    RedisRAII raii;
+                    boost::json::object src_obj;
+                    src_obj["key"] = block->key;
+                    src_obj["filename"] = block->filename;
+                    src_obj["checksum"] = block->checksum;
+                    src_obj["filepath"] = block->filePath;
+                    src_obj["last_seq"] = block->last_sequence;
+                    src_obj["cur_seq"] = block->curr_sequence;
+                    src_obj["transfered_size"] = block->transfered_size;
+                    src_obj["current_block_size"] = block->current_block_size;
+                    src_obj["total_size"] = block->total_size;
+                    src_obj["EOF"] = block->isEOF;
 
-    m_checksum2FileDesc.insert(std::pair(checksum, block));
-  }
+                    return raii->get()->setValueExp(block->key, boost::json::serialize(src_obj), 3000);
+          }
 
   [[nodiscard]]
   std::optional<std::shared_ptr<const handler::FileHasherDesc>>
-  getFileDescBlock(const std::string &checksum) {
-    typename ContainerType::accessor accessor;
-    if (!m_checksum2FileDesc.find(accessor, checksum))
-      return std::nullopt;
-    return accessor->second;
-  }
+            getFileDescBlock(const std::string& key) {
 
-private:
-  ContainerType m_checksum2FileDesc;
+            RedisRAII raii;
+
+            if (key.empty()) {
+                      return {};
+            }
+
+            //Search For Info Cache in Redis
+            auto opt = raii->get()->checkValue(key);
+            if (!opt.has_value()) {
+                      return {};
+            }
+
+            /*parse cache data inside Redis*/
+            try {
+                      boost::json::object obj = boost::json::parse(opt.value()).as_object();
+
+                      return std::make_shared<const handler::FileHasherDesc>(
+                                boost::json::value_to<std::string>(obj["filename"]),
+                                boost::json::value_to<std::string>(obj["checksum"]),
+                                boost::json::value_to<std::string>(obj["filepath"]),
+                                boost::json::value_to<std::string>(obj["cur_seq"]),
+                                boost::json::value_to<std::string>(obj["last_seq"]),
+                                boost::json::value_to<std::string>(obj["EOF"]),
+                                obj["transfered_size"].as_uint64(),
+                                obj["current_block_size"].as_uint64(),
+                                obj["total_size"].as_uint64());
+            }
+            catch (const boost::json::system_error& e) {
+                      spdlog::error("Failed to parse json data!");
+                      return std::nullopt;
+            }
+  }
 };
 
 #endif //_FILEHASHERLOGGER_HPP_

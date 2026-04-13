725d23a9adfb31050c9b8a948ca6e85046770fd3
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2026-04-13 23:09:39 +1000

    1.[MODIFY]:now serve as a signal rely to forward the signal of executor to else where 2.[ADD]:add signal to update interfaces avatar icons 3.[REMOVE]:remove md5 cache

--- chatting-client/logicmethod.cpp
+++ chatting-client/logicmethod.cpp
@@ -21,91 +21,29 @@ void LogicMethod::registerMetaType() {
   qRegisterMetaType<std::shared_ptr<QFileInfo>>("std::shared_ptr<QFileInfo>");
 }
 
-void LogicMethod::slot_resources_logic_handler(const uint16_t id, QJsonObject json)
-{
-    if(static_cast<ServiceType>(id) == ServiceType::SERVICE_FILEUPLOADRESPONSE ||
-        static_cast<ServiceType>(id) == ServiceType::SERVICE_FILECHECKUPLOADPROGRESSRESPONSE){
-
-        if (!json.contains("error")) {
-            qDebug() << "Json Parse Error!";
-            return;
-        }
-        if (json["error"].toInt() !=
-            static_cast<int>(ServiceStatus::SERVICE_SUCCESS)) {
-            qDebug() << "File Upload Failed!!";
-            return;
-        }
-
-        [[maybe_unused]] auto checksum = json["checksum"].toString();
-        [[maybe_unused]] auto curr_seq = json["curr_seq"].toString().toUInt();
-        [[maybe_unused]] auto curr_size = json["curr_size"].toString().toUInt();
-        [[maybe_unused]] auto total_size = json["total_size"].toString().toUInt();
-        [[maybe_unused]] auto eof = json["EOF"].toBool();
-
-        emit signal_break_point_resume(checksum, curr_seq, curr_size, total_size, eof);
-
-    }else{
-        qDebug() << "Invalid Resources Service ID!";
-    }
-}
-
 void LogicMethod::registerSignals() {
 
   /*incoming signal from FileTcpNetwork class*/
   connect(FileTCPNetwork::get_instance().get(),
-          &FileTCPNetwork::signal_resources_logic_handler,
-          this,
-          &LogicMethod::slot_resources_logic_handler);
-
-  connect(this, &LogicMethod::signal_break_point_resume, m_exec,
-          &LogicExecutor::slot_break_point_resume);
+          &FileTCPNetwork::signal_breakpoint_upload,
+          m_exec,
+          &LogicExecutor::slot_breakpoint_upload);
 
   //forward signal to executor
-  connect(this, &LogicMethod::signal_start_file_transmission, m_exec,
-          &LogicExecutor::signal_start_file_transmission);
+  connect(this, &LogicMethod::signal_start_file_upload, m_exec,
+          &LogicExecutor::signal_start_file_upload);
 
-  connect(this, &LogicMethod::signal_pause_file_transmission, m_exec,
-          &LogicExecutor::signal_pause_file_transmission);
+  connect(this, &LogicMethod::signal_pause_file_upload, m_exec,
+          &LogicExecutor::signal_pause_file_upload);
 
-  connect(this, &LogicMethod::signal_resume_file_transmission, m_exec,
-          &LogicExecutor::signal_resume_file_transmission);
+  connect(this, &LogicMethod::signal_resume_file_upload, m_exec,
+          &LogicExecutor::signal_resume_file_upload);
+
+  //update all UI interfaces that relevant to avatar icons(qlabels)
+  connect(m_exec,&LogicExecutor::signal_update_interfaces_avatar_icons,
+          this, &LogicMethod::signal_update_interfaces_avatar_icons);
 
   connect(m_thread, &QThread::finished, m_thread, &QObject::deleteLater);
-}
-
-void LogicMethod::recordMD5Progress(const QString &md5,
-                                    std::shared_ptr<QFileInfo> info) {
-
-  std::lock_guard<std::mutex> _lckg(m_mtx);
-
-  auto it = m_md5_cache.find(md5);
-
-  // insert new element
-  if (it == m_md5_cache.end()) {
-
-    auto [_, status] = m_md5_cache.try_emplace(md5, info);
-    if (!status)
-      qDebug() << "md5 uuid exist!\n";
-
-    return;
-  }
-
-  // Update Only
-  if (it->second) {
-    it->second.reset();
-  }
-  it->second = info;
-}
-
-std::optional<std::shared_ptr<QFileInfo>>
-LogicMethod::getFileByMD5(const QString &md5) {
-
-  std::lock_guard<std::mutex> _lckg(m_mtx);
-  auto it = m_md5_cache.find(md5);
-  if (it == m_md5_cache.end()) {
-    return std::nullopt;
-  }
-  return it->second;
 }
 
 bool LogicMethod::getPauseStatus() const { return m_pause; }
--- chatting-client/logicmethod.h
+++ chatting-client/logicmethod.h
@@ -1,15 +1,11 @@
 #pragma once
 #ifndef LOGICMETHOD_H
 #define LOGICMETHOD_H
-
-#include <QFileInfo>
 #include <QObject>
 #include <QThread>
 #include <atomic>
 #include <logicexecutor.h> /*execute some specfic logic from network*/
-#include <optional>
-#include <singleton.hpp>
-#include <unordered_map>
+#include <resourcestoragemanager.h>
 
 class LogicMethod : public QObject, public Singleton<LogicMethod> {
 
@@ -22,10 +18,6 @@ public:
 public:
   bool getPauseStatus() const;
   void setPause(const bool status);
-  void recordMD5Progress(const QString &md5, std::shared_ptr<QFileInfo> info);
-
-  [[nodiscard]]
-  std::optional<std::shared_ptr<QFileInfo>> getFileByMD5(const QString &md5);
 
 private:
   explicit LogicMethod(QObject *parent = nullptr);
@@ -34,29 +26,20 @@ private:
 
 signals:
   // start transmission(with init filename & filepath)
-  void signal_start_file_transmission(const QString &fileName,
+  void signal_start_file_upload(const QString &fileName,
                                       const QString &filePath,
                                       const std::size_t fileChunk = 4096);
 
   // pause transmission
-  void signal_pause_file_transmission();
+  void signal_pause_file_upload();
 
   // resume transmission
-  void signal_resume_file_transmission();
+  void signal_resume_file_upload(const QString &fileName,const QString &filePath);
 
-  /*
-   * signal_break_point_resume could serve for two main purposes
-   * - indicate the process of file upload response, and start to prepare for the next block!
-   * - when user activate break point resume, it will start from the curr_size of the file
-   */
-  void signal_break_point_resume(QString checksum,
-                                       const std::size_t curr_seq,
-                                       const std::size_t curr_size,
-                                       const std::size_t total_size,
-                                       const bool eof);
+  //update all UI interfaces that relevant to avatar icons(qlabels)
+  void signal_update_interfaces_avatar_icons(const QString& path);
 
 private slots:
-  void slot_resources_logic_handler(const uint16_t id, QJsonObject obj);
 
 private:
   QThread *m_thread;
@@ -64,15 +47,6 @@ private:
 
   // transmission stopped
   std::atomic<bool> m_pause = false;
-
-  // mutex
-  std::mutex m_mtx;
-
-  // store info in mapping structure
-  std::unordered_map<
-      /*    md5 = */ QString,
-      /*QFileInfo=*/std::shared_ptr<QFileInfo>>
-      m_md5_cache;
 };
 
 #endif // LOGICMETHOD_H

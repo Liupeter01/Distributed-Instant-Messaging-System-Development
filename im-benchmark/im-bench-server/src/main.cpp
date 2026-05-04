8a53b94a48d176501edf6ffa1c2866adaa6efd05
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2026-05-04 22:18:21 +1000

    1.[ADD]:add quit server by ctrl-c 2.[ADD]:add init io service pool at the beginning and allocate an io_context for echoserver connector

--- im-benchmark/im-bench-server/src/main.cpp
+++ im-benchmark/im-bench-server/src/main.cpp
@@ -1,16 +1,22 @@
 #include <echo_server.h>
+#include <IOServicePool.hpp>
 
 int main(int argc, char** argv) {
 
-          uint16_t port = argc > 1 ? std::atoi(argv[1]) : 8888;
-          int nthreads = argc > 2 ? std::atoi(argv[2]) : 4;
+          auto& service_pool = IOServicePool::get_instance();
+          uint16_t port = 8888;
 
-          io_context ioc;
-          EchoServer srv(ioc, port);
+          boost::asio::io_context ioc;
+          boost::asio::signal_set signal{ ioc, SIGINT, SIGTERM };
+          signal.async_wait([&ioc, &service_pool](boost::system::error_code ec, int sig_number) {
+                    if (ec) {
+                              return;
+                    }
+                    ioc.stop();
+                    service_pool->shutdown();
+                    });
 
-          std::vector<std::thread> threads;
-          for (int i = 0; i < nthreads; i++) {
-                    threads.emplace_back([&ioc] { ioc.run(); });
-          }
-          for (auto& t : threads) t.join();
+          EchoServer srv(service_pool->getIOServiceContext(), port);
+          srv.startAccept();
+          ioc.run();
 }
